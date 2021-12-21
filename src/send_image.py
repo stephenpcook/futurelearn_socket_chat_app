3ad0bd86892a9ee5f692f723b556fc1d8e91d64c
Stephen P. Cook <s.cook@seiche.com>
2021-12-21 13:13:41 +0000

    Update send_image to test error detection

--- src/send_image.py
+++ src/send_image.py
@@ -5,6 +5,8 @@ from itertools import product
 from pathlib import Path
 import socket
 import pickle
+from time import sleep
+from random import randint, seed
 
 from fl_networking_tools import ImageViewer
 from PIL import Image
@@ -17,11 +19,22 @@ def do_send():
     image = Image.open(f)
     width, height = image.size
 
+    seed(0)
+    dropped = 0
+
+    n_slowdown = 1000
+    slowdownplease = []
     for pos in product(range(width), range(height)):
+        if not(randint(0, 9)):
+            dropped += 1
+            continue
         pixel = image.getpixel(pos)
         data = pickle.dumps((pos, pixel))
         udp_client.sendto(data, ('127.0.0.1', 20001))
+        for _ in range(n_slowdown):
+            slowdownplease.append('Hello')
     udp_client.close()
+    print(dropped)
 
 
 def do_recv():
@@ -31,18 +44,36 @@ def do_recv():
 
     def get_pixel_data():
         lost_pixels = 0
-        last_pos = (-1, -1)
+        current_column = 0
+        all_column_pixels = []
+        column_pixels = 0
+        old_pos = (0,0)
+        max_column = 0
         try:
             while True:
                 data, _ = udp_server.recvfrom(1024)
                 pos, rgb = pickle.loads(data)
-                lost_pixel = (((pos[0] - last_pos[0]) > 1)
-                              or ((pos[1] - last_pos[1]) > 1))
-                if lost_pixel:
-                    lost_pixels += 1
+                column_pixels += 1
                 viewer.put_pixel(pos, rgb)
                 viewer.text = str(lost_pixels)
-                last_pos = pos
+                # End of column, update missing pixels (does not work on last
+                # column, or pixels arriving out of order)
+                if (pos[0] > current_column):
+                    # The most recent pixel was from the next column
+                    all_column_pixels.append(column_pixels - 1)
+                    # Check if we've missed an entire column
+                    col_jump = current_column - pos[0]
+                    if (col_jump > 1):
+                        all_column_pixels.update([0] * (col_jump - 1))
+                    # Check for a better estimate of the column height
+                    max_column = max(old_pos[1], max_column)
+                    expected_pixels = len(all_column_pixels) * max_column
+                    lost_pixels = expected_pixels - sum(all_column_pixels)
+                    # Setup for next column
+                    current_column = pos[0]
+                    # Don't forget the current pixel (so 1, not 0)
+                    column_pixels = 1
+                old_pos = pos
         except KeyboardInterrupt:
             pass
 
